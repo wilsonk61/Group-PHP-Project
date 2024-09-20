19b0570c5892db7c33a6425b9e22d5b7991d8a0b
wilsonk61 <wilsonk61@mymail.nku.edu>
2024-09-19 21:50:55 -0400

    Update detail.php

--- detail.php
+++ detail.php
@@ -403,6 +403,7 @@ $person = $team[$id];
 
 
 </head> 
+
 <body>
     <article class="resume-wrapper text-center position-relative">
 		<?php /* Only the following line changed from the file in the previous assignment */ ?>
@@ -411,23 +412,23 @@ $person = $team[$id];
 		    <header class="resume-header pt-4 pt-md-0">
 			    <div class="row">
 				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
-				        <img class="picture" src="<?= $person['image']; ?>" alt="">
+				        <img class="picture" src="assets/images/profile.jpg" alt="">
 				    </div><!--//col-->
 				    <div class="col">
 					    <div class="row p-4 justify-content-center justify-content-md-between">
 						    <div class="primary-info col-auto">
-							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase"><?= $person['name'] ?></h1>
-							    <div class="title mb-3"><?= $person['role']?></div>
+							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">Your name</h1>
+							    <div class="title mb-3">Your desired job title</div>
 							    <ul class="list-unstyled">
-								    <li class="mb-2"><a class="text-link" href="#"><i class="far fa-envelope fa-fw me-2" data-fa-transform="grow-3"></i><?= $person['email']?></a></li>
-								    <li><a class="text-link" href="#"><i class="fas fa-mobile-alt fa-fw me-2" data-fa-transform="grow-6"></i><?= $person['phone']?></a></li>
+								    <li class="mb-2"><a class="text-link" href="#"><i class="far fa-envelope fa-fw me-2" data-fa-transform="grow-3"></i>your@email.com</a></li>
+								    <li><a class="text-link" href="#"><i class="fas fa-mobile-alt fa-fw me-2" data-fa-transform="grow-6"></i>0123 456 78900</a></li>
 							    </ul>
 						    </div><!--//primary-info-->
 						    <div class="secondary-info col-auto mt-2">
 							    <ul class="resume-social list-unstyled">
-					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-linkedin-in fa-fw"></i></span><?= $person['linkedin']?></a></li>
-					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-github-alt fa-fw"></i></span><?= $person['github']?></a></li>
-					                <li><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fas fa-globe"></i></span><?= $person['website']?></a></li>
+					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-linkedin-in fa-fw"></i></span>linkedin.com/in/yourlink</a></li>
+					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-github-alt fa-fw"></i></span>github.com/yourhandle</a></li>
+					                <li><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fas fa-globe"></i></span>yourwebsite.com</a></li>
 							    </ul>
 						    </div><!--//secondary-info-->
 					    </div><!--//row-->
@@ -439,7 +440,7 @@ $person = $team[$id];
 			    <section class="resume-section summary-section mb-5">
 				    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Summary</h2>
 				    <div class="resume-section-content">
-					    <p class="mb-0"><?= $person['summary']?></p>
+					    <p class="mb-0">Summarise your education and professional experience here. Add a couple of fun facts. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque congue elit ut nisi vehicula iaculis. Integer porta nisi erat, quis gravida quam dignissim ut. Nullam tincidunt mollis finibus. Vestibulum et diam vel tellus blandit convallis non id mauris. Curabitur feugiat tincidunt ante, ut iaculis sem. Sed eleifend fringilla diam, quis vehicula tellus fringilla sed. In sagittis commodo ipsum pulvinar sagittis. Ut et turpis sit amet erat elementum convallis ac eu ipsum. Aenean varius eget mi in mollis. Integer tempus diam libero, id blandit neque aliquam non. Maecenas eleifend leo ut pellentesque bibendum. Phasellus consectetur facilisis nunc, at ultricies nisi eleifend eget. Fusce molestie et orci non pulvinar. Aenean ac tristique orci, vitae viverra mi.</p>
 				    </div>
 			    </section><!--//summary-section-->
 			    <div class="row">
@@ -447,7 +448,12 @@ $person = $team[$id];
 					    <section class="resume-section experience-section mb-5">
 						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Work Experience</h2>
 						    <div class="resume-section-content">
-							    <div class="resume-timeline position-relative">
+
+
+									<!--WHAT I DID FOR MULTIDIMENSIONAL ARRAY USING FOREACH-->
+									
+									
+								<div class="resume-timeline position-relative">
 									<?php foreach ($person['work_experience'] as $experience): ?>
 										<article class="resume-timeline-item position-relative pb-5">
 											<div class="resume-timeline-item-header mb-2">
@@ -473,8 +479,11 @@ $person = $team[$id];
 												</ul>
 											</div><!--//resume-timeline-item-desc-->
 										</article><!--//resume-timeline-item-->
-									<?php endforeach; ?>								    
-							    </div><!--//resume-timeline-->						    
+									<?php endforeach; ?> 
+							    </div><!--//resume-timeline-->
+								
+								
+
 						    </div>
 					    </section><!--//projects-section-->
 				    </div>
@@ -484,22 +493,51 @@ $person = $team[$id];
 						    <div class="resume-section-content">
 						        <div class="resume-skill-item">
 							        <ul class="list-unstyled mb-4">
-								        <?php foreach ($person['skills'] as $skill): ?>
-											<li class="mb-2">
-												<div class="resume-skill-name"><?= $skill['name'] ?></div>
-												<div class="progress resume-progress">
-													<div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: <?= $skill['progress'] ?>%" aria-valuenow="<?= $skill['progress'] ?>" aria-valuemin="0" aria-valuemax="100"></div>
-												</div>
-											</li>
-										<?php endforeach; ?>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">Angular</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 98%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">React</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 94%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">JavaScript</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 96%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        
+								        <li class="mb-2">
+								            <div class="resume-skill-name">Node.js</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 92%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">HTML/CSS/SASS/LESS</div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 96%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
 							        </ul>
 						        </div><!--//resume-skill-item-->
 						        <div class="resume-skill-item">
 						            <h4 class="resume-skills-cat font-weight-bold">Others</h4>
 						            <ul class="list-inline">
-							            <?php foreach ($person['other_skills'] as $other_skill): ?>
-											<li class="list-inline-item"><span class="badge badge-light"><?= $other_skill ?></span></li>
-										<?php endforeach; ?>
+							            <li class="list-inline-item"><span class="badge badge-light">DevOps</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Code Review</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Git</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Unit Testing</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Wireframing</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Sketch</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Balsamiq</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">WordPress</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Shopify</span></li>
 						            </ul>
 						        </div><!--//resume-skill-item-->
 						    </div><!--resume-section-content-->
@@ -508,13 +546,16 @@ $person = $team[$id];
 						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Education</h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled">
-								    <?php foreach ($person['education'] as $education): ?>
-										<li class="mb-2">
-											<div class="resume-degree font-weight-bold"><?= $education['degree'] ?></div>
-											<div class="resume-degree-org"><?= $education['organization'] ?></div>
-											<div class="resume-degree-time"><?= $education['time'] ?></div>
-										</li>
-									<?php endforeach; ?>
+								    <li class="mb-2">
+								        <div class="resume-degree font-weight-bold">MSc in Computer Science</div>
+								        <div class="resume-degree-org">University College London</div>
+								        <div class="resume-degree-time">2013 - 2014</div>
+								    </li>
+								    <li>
+								        <div class="resume-degree font-weight-bold">BSc Maths and Physics</div>
+								        <div class="resume-degree-org">Imperial College London</div>
+								        <div class="resume-degree-time">2010 - 2013</div>
+								    </li>
 							    </ul>
 						    </div>
 					    </section><!--//education-section-->
@@ -522,14 +563,16 @@ $person = $team[$id];
 						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Awards</h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled resume-awards-list">
-								    
-									<?php foreach ($person['awards'] as $award): ?>
-										<li class="mb-2 ps-4 position-relative">
-											<i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
-											<div class="resume-award-name"><?= $award['name'] ?></div>
-											<div class="resume-award-desc"><?= $award['description'] ?></div>
-										</li>
-									<?php endforeach; ?>
+								    <li class="mb-2 ps-4 position-relative">
+								        <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
+								        <div class="resume-award-name">Award Name Lorem</div>
+								        <div class="resume-award-desc">Award desc goes here, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo.</div>
+								    </li>
+								    <li class="mb-0 ps-4 position-relative">
+								        <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
+								        <div class="resume-award-name">Award Name Ipsum</div>
+								        <div class="resume-award-desc">Award desc goes here, ultricies nec, pellentesque.</div>
+								    </li>
 							    </ul>
 						    </div>
 					    </section><!--//interests-section-->
@@ -537,9 +580,9 @@ $person = $team[$id];
 						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Languages</h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled resume-lang-list">
-									<?php foreach ($person['languages'] as $language): ?>
-										<li class="mb-2"><span class="resume-lang-name font-weight-bold"><?= $language['name'] ?></span> <small class="text-muted font-weight-normal">(<?= $language['proficiency'] ?>)</small></li>
-									<?php endforeach; ?>
+								    <li class="mb-2"><span class="resume-lang-name font-weight-bold">English</span> <small class="text-muted font-weight-normal">(Native)</small></li>
+								    <li class="mb-2 align-middle"><span class="resume-lang-name font-weight-bold">French</span> <small class="text-muted font-weight-normal">(Professional)</small></li>
+								    <li><span class="resume-lang-name font-weight-bold">Spanish</span> <small class="text-muted font-weight-normal">(Professional)</small></li>
 							    </ul>
 						    </div>
 					    </section><!--//language-section-->
@@ -547,37 +590,63 @@ $person = $team[$id];
 						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Interests</h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled">
-								<?php foreach ($person['interests'] as $interest): ?>
-									<li class="mb-1"><?= $interest ?></li>
-								<?php endforeach; ?>
+								    <li class="mb-1">Climbing</li>
+								    <li class="mb-1">Snowboarding</li>
+								    <li class="mb-1">Cooking</li>
 							    </ul>
 						    </div>
-					    </section><!--//interests-section--> 
+					    </section><!--//interests-section-->
+					    
 				    </div>
 			    </div><!--//row-->
 				<section class="resume-section experience-section mb-5">
 					<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Projects</h2>
 					<div class="row mt-4">
-						<?php foreach ($person['projects'] as $project): ?>
-							<div class="col-md-4">
-								<div class="card">
-									<img src="<?= $project['image'] ?>" alt="<?= $project['title'] ?>" class="card-img-top">
-									<div class="card-body">
-										<h5 class="card-title"><?= $project['title'] ?></h5>
-										<p class="card-text"><?= $project['description'] ?></p>
-										<a href="<?= $project['link'] ?>" class="btn btn-outline-primary">Go to link</a>
-									</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img src="path-to-project-image1.jpg" alt="Project 1" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title">Project 1</h5>
+									<p class="card-text">Brief description of Project 1.</p>
+									<a href="btn btn-outline-primary" href="#">Go to link</a>
 								</div>
 							</div>
-						<?php endforeach; ?>						
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img src="path-to-project-image2.jpg" alt="Project 2" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title">Project 2</h5>
+									<p class="card-text">Brief description of Project 2.</p>
+									<a href="btn btn-outline-primary" href="#">Go to link</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img src="path-to-project-image3.jpg" alt="Project 3" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title">Project 3</h5>
+									<p class="card-text">Brief description of Project 3.</p>
+									<a href="btn btn-outline-primary" href="#">Go to link</a>
+								</div>
+							</div>
+						</div>
 					</div>
 				</section><!--//projects-section-->
-		    </div><!--//resume-body--> 
+		    </div><!--//resume-body-->
+		    
+		    
 	    </div>
     </article> 
+
+    
     <footer class="footer text-center pt-2 pb-5">
 	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
         <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> by Your names</small>
     </footer>
+
+    
+
 </body>
 </html> 
